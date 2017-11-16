8ef0fbdc457e547ebf16453ebde916548cdfc9aa
Macker67 <andrewpmcb@gmail.com>
2017-11-16 11:51:59 +0000

    Add files via upload

---
+++ POS/src/pos/Restaurant.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos;
+
+/**
+ *
+ * @author x16482412
+ */
+public class Restaurant extends POS {
+    private 
+    
+}
